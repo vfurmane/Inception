b6d3527679dab3213ff98be118526f1de6ca8419
vfurmane <vfurmane@student.42.fr>
2021-06-08 15:22:03 +0200

    Use my own PHP image

--- srcs/config/wordpress/docker-entrypoint.sh
+++ srcs/config/wordpress/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -Ei 's/listen = \/run\/php\/php7\.3-fpm\.sock/listen = 127.0.0.1:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/listen[[:blank:]]*=[[:blank:]]*.*/listen = 0.0.0.0:9000/" /etc/php/7.3/fpm/pool.d/www.conf
 
 exec "$@"
