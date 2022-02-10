37f3b7c1e7b3ef776931262f81266be96d57d870
vfurmane <vfurmane@student.42.fr>
2022-02-10 15:59:25 +0100

    feat: create the PHP image

--- srcs/wordpress/wp-config-sample.php
+++ srcs/wordpress/wp-config-sample.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
 define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
