2b985f547c63991c8c17f50ee3157a95076f9a7a
vfurmane <vfurmane@student.42.fr>
2022-02-14 11:32:09 +0100

    feat: move $DOMAIN_NAME into .env

--- srcs/proxy/docker-entrypoint.sh
+++ srcs/proxy/docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-mkdir -p /etc/nginx/vfurmane.42.fr
-cd /etc/nginx/vfurmane.42.fr
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out /etc/nginx/vfurmane.42.fr/server.key
+
+sed -i "s/login\.42\.fr/$DOMAIN_NAME/g" /etc/nginx/http.d/default.conf
+mkdir -p /etc/nginx/$DOMAIN_NAME
+cd /etc/nginx/$DOMAIN_NAME
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out /etc/nginx/$DOMAIN_NAME/server.key
 chmod 400 server.key
-openssl req -new -subj "/C=FR/ST=Paris/L=Paris/O=Dis/CN=vfurmane.42.fr" -sha256 -key server.key -out /etc/nginx/vfurmane.42.fr/server.csr
-openssl x509 -req -days 1095 -in server.csr -signkey /etc/nginx/vfurmane.42.fr/server.key -out /etc/nginx/vfurmane.42.fr/server.crt;
+openssl req -new -subj "/C=FR/ST=Paris/L=Paris/O=Dis/CN=$DOMAIN_NAME" -sha256 -key server.key -out /etc/nginx/$DOMAIN_NAME/server.csr
+openssl x509 -req -days 1095 -in server.csr -signkey /etc/nginx/$DOMAIN_NAME/server.key -out /etc/nginx/$DOMAIN_NAME/server.crt;
 
 echo "Starting NGINX..."
 exec "$@"
