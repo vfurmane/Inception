9893c91f4445ec56b0e17e64734d20ba1975ff5f
vfurmane <vfurmane@student.42.fr>
2022-02-15 10:52:55 +0100

    feat: add redis cache for Wordpress

--- srcs/wordpress/docker-entrypoint.sh
+++ srcs/wordpress/docker-entrypoint.sh
@@ -6,6 +6,8 @@ if ! [ -f /var/www/html/.wp-ok ]
 then
 	wp core install --url=vfurmane.42.fr --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=/var/www/html
 	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD --porcelain --path=/var/www/html
+	wp plugin install redis-cache --activate --path=/var/www/html
+	wp redis enable --path=/var/www/html
 	touch /var/www/html/.wp-ok
 fi
 chown -R www-data:www-data /var/www/html
--- srcs/wordpress/wp-config-sample.php
+++ srcs/wordpress/wp-config-sample.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+/** The redis cache host */
+define( 'WP_REDIS_HOST', getenv('WORDPRESS_REDIS_HOST') );
+
 /**#@+
  * Authentication unique keys and salts.
  *
