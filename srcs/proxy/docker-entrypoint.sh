986868d976ec29fea2613c7d7dae7ee5b4187f08
vfurmane <vfurmane@student.42.fr>
2022-03-08 12:12:14 +0100

    feat: add a portfolio subdomain

--- srcs/proxy/docker-entrypoint.sh
+++ srcs/proxy/docker-entrypoint.sh
@@ -20,6 +20,12 @@ openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out /etc/nginx/gra
 chmod 400 server.key
 openssl req -new -subj "/C=FR/ST=Paris/L=Paris/O=Dis/CN=grafana.$DOMAIN_NAME" -sha256 -key server.key -out /etc/nginx/grafana.$DOMAIN_NAME/server.csr
 openssl x509 -req -days 1095 -in server.csr -signkey /etc/nginx/grafana.$DOMAIN_NAME/server.key -out /etc/nginx/grafana.$DOMAIN_NAME/server.crt;
+mkdir -p /etc/nginx/portfolio.$DOMAIN_NAME
+cd /etc/nginx/portfolio.$DOMAIN_NAME
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out /etc/nginx/portfolio.$DOMAIN_NAME/server.key
+chmod 400 server.key
+openssl req -new -subj "/C=FR/ST=Paris/L=Paris/O=Dis/CN=portfolio.$DOMAIN_NAME" -sha256 -key server.key -out /etc/nginx/portfolio.$DOMAIN_NAME/server.csr
+openssl x509 -req -days 1095 -in server.csr -signkey /etc/nginx/portfolio.$DOMAIN_NAME/server.key -out /etc/nginx/portfolio.$DOMAIN_NAME/server.crt;
 
 echo "Starting NGINX..."
 exec "$@"
